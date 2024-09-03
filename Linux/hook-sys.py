5c4288c2fe20a27abb1f425bf0e09975ae5492ed
annoyingapt <55898651+annoyingapt@users.noreply.github.com>
2024-09-04 11:42:50 +1200

    Update hook-sys.py

--- Linux/hook-sys.py
+++ Linux/hook-sys.py
@@ -1,6 +1,6 @@
-from lazagne.config.manage_modules import get_modules_names
-from lazagne.softwares.browsers.chromium_browsers import chromium_based_module_location
-from lazagne.softwares.browsers.firefox_browsers import mozilla_module_location
+from pasta.config.manage_modules import get_modules_names
+from pasta.softwares.browsers.chromium_browsers import chromium_based_module_location
+from pasta.softwares.browsers.firefox_browsers import mozilla_module_location
 
 all_hidden_imports_module_names = get_modules_names() + [mozilla_module_location, chromium_based_module_location]
 hiddenimports = [package_name for package_name, module_name in all_hidden_imports_module_names]
